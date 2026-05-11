d455ad868426cdc44ce8452915013c669beba210
Kaydensambeconsulting <kayden.padayachee@sambeconsulting.com>
2026-05-11 15:44:50 +0200

    Delete and truncate

---
+++ Database_Administration/Truncate.sql
@@ -0,0 +1,15 @@
+USE [AdventureWorks2016]
+GO
+
+INSERT INTO [dbo].[employeenew]
+           ([employeename])
+     VALUES
+           ('James')
+GO
+
+
+select * from employeenew
+
+delete  from employeenew
+
+truncate table employeenew
