4cbede87021e4234b1ad5a03466872a2a827f8d5
Kaydensambeconsulting <kayden.padayachee@sambeconsulting.com>
2026-06-03 14:28:07 +0200

    Section 48

---
+++ Database_Administration/T-SQL/Nested.sql
@@ -0,0 +1,26 @@
+select * 
+from [dbo].[tblDepartment] as D
+left join [dbo].[tblEmployee] as E
+on D.Department = E.Department
+
+select D.Department, D.DepartmentHead, E.EmployeeNumber, E.EmployeeFirstName, E.EmployeeLastName 
+from [dbo].[tblDepartment] as D
+left join [dbo].[tblEmployee] as E
+on D.Department = E.Department
+
+
+select D.Department, D.DepartmentHead, E.EmployeeNumber, E.EmployeeFirstName, E.EmployeeLastName 
+from [dbo].[tblDepartment] as D
+left join [dbo].[tblEmployee] as E
+on D.Department = E.Department
+where D.Department = 'HR'
+
+select *
+from [dbo].[tblEmployee] as E
+left join [dbo].[tblTransaction] as T
+on E.EmployeeNumber = T.EmployeeNumber
+
+select E.EmployeeNumber, T.Amount
+from [dbo].[tblEmployee] as E
+left join [dbo].[tblTransaction] as T
+on E.EmployeeNumber = T.EmployeeNumber
---
+++ Database_Administration/T-SQL/include clients.sql
@@ -0,0 +1,7 @@
+select *
+from [dbo].[tblEmployee] as E
+
+select *
+from [dbo].[tblEmployee] as E
+where E.EmployeeNumber = 134 
+
